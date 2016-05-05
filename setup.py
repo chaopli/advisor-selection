f92a6beb3c7d7a5ebad9033a0377d13c6cd23ece
Chao Li <cli@avinetworks.com>
2016-05-04 21:44:30 -0700

    setup virtual env

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ virtenv = os.environ['OPENSHIFT_REPO_DIR']
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
 os.chdir(os.environ['OPENSHIFT_REPO_DIR'])
 call('virtualenv venv'.split())
-print os.getcwd()
-call('source ./venv/bin/activate')
+call('source ./venv/bin/activate'.split())
 call('pip install django'.split())
 setup(name='mysite',
       version='1.0',
