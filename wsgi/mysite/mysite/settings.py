102a10a039d0718f12807541ce218b6b83c5c4c6
Chao Li <cli@avinetworks.com>
2016-05-04 22:12:07 -0700

    setup virtual env

--- wsgi/mysite/mysite/settings.py
+++ wsgi/mysite/mysite/settings.py
@@ -15,6 +15,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+WSGI_DIR = os.path.dirname(BASE_DIR)
+REPO_DIR = os.path.dirname(WSGI_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -120,11 +122,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-PROJECT_ROOT = os.path.normpath(os.path.dirname(file))
 STATIC_URL = '/static/'
-DJANGO_PATH = os.dirname(django.__file__)
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    os.path.join(PROJECT_ROOT, 'static'),
-    os.path.join(DJANGO_PATH, 'contrib/admin/static')
-]
+STATIC_ROOT = os.path.join(WSGI_DIR, 'static')
+DATA_DIR = os.environ.get('OPENSHIFT_DATA_DIR', BASE_DIR)
