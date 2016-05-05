5f0b3960f0ab3df3a63a445f984a45cb70686309
Chao Li <cli@avinetworks.com>
2016-05-04 21:38:45 -0700

    setup virtualenv before hand

--- setup.py
+++ setup.py
@@ -6,11 +6,12 @@ virtenv = os.environ['OPENSHIFT_REPO_DIR']
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
 os.chdir(os.environ['OPENSHIFT_REPO_DIR'])
 call('virtualenv venv'.split())
+call('pip install django'.split())
 setup(name='mysite',
       version='1.0',
       description='OpenShift App',
       author='Chao',
       author_email='chaopli@outlook.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['Django>=1.3'],
+      #install_requires=['Django>=1.3'],
      )
