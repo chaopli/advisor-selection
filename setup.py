bb1a8a63b563e6db3fbbbb9e2f98928317a94a6c
Chao Li <cli@avinetworks.com>
2016-05-04 18:00:20 -0700

    fix

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='mysite',
       version='1.0',
       description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      author='Chao',
+      author_email='chaopli@outlook.com',
       url='http://www.python.org/sigs/distutils-sig/',
       install_requires=['Django>=1.3'],
      )
