3a5ffeca8cdf1ff2b28c92459f324daca251f29f
Chao Li <cli@avinetworks.com>
2016-05-19 18:19:19 -0700

    Added routing for empty url update db

--- wsgi/mysite/mysite/urls.py
+++ wsgi/mysite/mysite/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    url(r'', include('polls.urls')),
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
